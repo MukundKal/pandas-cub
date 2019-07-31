371b14e946903578f7562a39473315c9a1545bc8
Mukund <mukundkalra@gmail.com>
2019-07-31 12:43:10 +0530

    single element selection of the DF using brackets

--- pandas_cub/__init__.py
+++ pandas_cub/__init__.py
@@ -297,7 +297,25 @@ class DataFrame:
     def _getitem_tuple(self, item):
         # simultaneous selection of rows and cols -> df[rs, cs]
         if len(item) !=2:
-        	raise ValueError("item tuple must have length 2")
+        	raise ValueError("item tuple must have length 2  ")
+
+        row_selection, col_selection = item
+
+        if isinstance(row_selection,int):
+        	row_selection = [row_selection]
+
+        if isinstance(col_selection, int):
+        	col_selection = [self.columns[col_selection]]
+        
+        elif isinstance(col_selection,str):
+        	col_selection = [col_selection]
+        	
+
+        new_data={}
+        for col in col_selection:
+        	new_data[col] = self._data[col][row_selection]
+
+        return DataFrame(new_data)
 
 
     def _ipython_key_completions_(self):
