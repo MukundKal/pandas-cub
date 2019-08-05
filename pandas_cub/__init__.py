27246d93656f040532ef7ea4a76720f9f195252f
Mukund <mukundkalra@gmail.com>
2019-08-05 13:47:50 +0530

    added read_csv and closed.

--- pandas_cub/__init__.py
+++ pandas_cub/__init__.py
@@ -388,7 +388,7 @@ class DataFrame:
         return self._agg(np.argmax)
 
     def argmin(self):
-        return self._agg(np.argmin)
+        return self._agg(np.argmin) 
 
     def _agg(self, aggfunc):
         """
@@ -403,7 +403,16 @@ class DataFrame:
         -------
         A DataFrame
         """
-        pass
+        new_data ={}
+        for col, value in self._data.items():
+        	try:
+        		new_data[col] = np.array([aggfunc(value)])
+        	except TypeError:
+        		pass
+        return DataFrame(new_data)
+
+
+
 
     def isna(self):
         """
@@ -413,7 +422,17 @@ class DataFrame:
         -------
         A DataFrame of booleans the same size as the calling DataFrame
         """
-        pass
+        new_data = {}
+        for col,value in self._data.items():
+
+        	if value.dtype.kind == 'O':
+        		new_data[col] = value == None
+        	else:
+        		new_data[col] = np.isnan(value)
+
+        return DataFrame(new_data)
+
+
 
     def count(self):
         """
@@ -423,7 +442,15 @@ class DataFrame:
         -------
         A DataFrame
         """
-        pass
+        df = self.isna()
+        num_rows = len(df)
+
+        new_data = {}
+
+        for col,values in df._data.items():
+        	new_data[col]= np.array([num_rows - value.sum()])
+
+        return DataFrame(new_data)
 
     def unique(self):
         """
@@ -433,7 +460,7 @@ class DataFrame:
         -------
         A list of one-column DataFrames
         """
-        pass
+        
 
     def nunique(self):
         """
@@ -849,12 +876,22 @@ class StringMethods:
         return self._str_method(str.encode, col, encoding, errors)
 
     def _str_method(self, method, col, *args):
-        pass
+        value = self._df._data(col)
+        if value.dtype.kind != 'O':
+        	raise TypeError('str accessor only works with strs')
 
+        new_vals = []
+
+        for val in value:
+        	new_vals.append(method(val, *args))
+
+        return DataFrame({col: np.array(new_vals)})
 
 def read_csv(fn):
     """
     Read in a comma-separated value file as a DataFrame
+
+    Assume Header row is present.
 
     Parameters
     ----------
@@ -864,4 +901,35 @@ def read_csv(fn):
     -------
     A DataFrame
     """
-    pass
+    
+    from collections import defualtdict
+
+    data = defualtdict(list)
+
+    with open(fn) as f:
+    	header = f.readline() #comma separated string
+    	column_names = header.strip('\n').split(',') #column name list
+
+    	for line in f:  #next line after header
+			values = line.strip('\n').split(',')
+
+			for col, val in zip(column_names,values):
+				data[col].append(val)
+
+	new_data = {}
+	for col, vals in data.items():
+		try:
+			new_data[col] = np.array(vals, dtype='int')
+		except ValueError:
+			try:
+				new_data[col] = np.array(vals, dtype='float')
+			except:
+				new_data[col] = np.array(vals, dtype= 'object')
+
+
+	return DataFrame(new_data)
+
+	
+
+
+
